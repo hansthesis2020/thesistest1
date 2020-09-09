fae060bea0133984463adcc0dd9e5710cbec3d75
thomasbeyethesis <thomas.beyer.thesis@gmail.com>
2020-09-09 21:05:46 +0200

    init2

--- src/test/java/thesis/Test.java
+++ src/test/java/thesis/Test.java
@@ -1,5 +1,7 @@
 package thesis;
 
+import static org.junit.Assert.assertNull;
+
 import java.awt.Point;
 
 import org.assertj.swing.core.MouseButton;
@@ -22,18 +24,17 @@ public class Test extends AssertJSwingJUnitTestCase {
 
 	@org.junit.Test
 	public void testMethod() {
-		
-		
+
 		window.textBox("entertext").requireEmpty();
 		window.textBox("textarea").requireEmpty();
 
 		window.textBox("entertext").enterText("testtext");
 		window.button("sendbutton").click();
 		window.textBox("textarea").requireText("testtext");
-		
+
 //		robot().click(new Point(50, 50), MouseButton.LEFT_BUTTON, 1);
 
-	    try {
+		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
@@ -42,4 +43,10 @@ public class Test extends AssertJSwingJUnitTestCase {
 
 	}
 
+	@org.junit.Test
+	public void testWorks() {
+
+		assertNull(null);
+
+	}
 }
